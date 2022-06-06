0959055228b035c5cada7b7722ee51d2d5ce5dbb
Kateryna Hrytsaienko <=>
2022-06-06 14:58:14 +0300

    fix issue with unified access to bucket

--- main.tf
+++ main.tf
@@ -20,8 +20,6 @@ resource "google_storage_bucket" "cc-static-generated" {
   location      = var.region
   force_destroy = true
 
-  uniform_bucket_level_access = true
-
   website {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
